1a3cb3dd43137c4d4522ca7f845a6f38259e5168
Leon <978940343@qq.com>
2019-09-25 13:06:12 +0800

    pandas I/O order fix

--- time_mission.py
+++ time_mission.py
@@ -1,20 +1,20 @@
 import time
-import catch
-import get_download_url
-import image_save
-import send_email
 
 
 if __name__ == '__main__':
     flag = True
     while True:
         if time.localtime().tm_hour == 0 and flag:
+            import catch
             if not catch.main():
+                import get_download_url
                 get_download_url.main()
+                import image_save
                 left = image_save.main()
                 # while left != 0:
                 #     left = image_save.main(left)
                 time.sleep(3)
+                import send_email
                 send_email.main("gmail")
             flag = False
         elif time.localtime().tm_hour == 16 and not flag:
